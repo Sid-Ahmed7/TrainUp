a886993f7d89d8c7a3e2b3e75442bc173394fbf7
Sid-Ahmed7 <sido91080@hotmail.fr>
2025-07-02 22:43:06 +0200

    fix middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken';
 import { JWT } from "../types/JWT";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
 
-export const verifyToken = (req: Request, res: Response) => {
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
 
     const header = req.headers.authorization;
     
     if(!header || !header.startsWith("Bearer ")){
         res.status(401).json({error: "Token manquant ou mal formaté"});
-        return false;
+        return ;
     }
 
     const token = header.split(" ")[1];
@@ -23,10 +23,10 @@ export const verifyToken = (req: Request, res: Response) => {
       role: decodedJwt.role,
     };
 
-        return true;
+        next()
     } catch(erro){
         res.status(401).json({error: "Token invalide ou expiré"});
-        return false;
+        return ;
     }
 
 
--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -3,10 +3,11 @@ import * as categoryController from "../controllers/categoryController";
 import { verifyRoles } from "../middlewares/verifyRoles";
 
 import { Role } from "../enums/Role";
+import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = express.Router()
 
-router.post("/nouvelle",verifyRoles(Role.SUPER_ADMIN),categoryController.createCategory)
+router.post("/nouvelle", verifyToken,verifyRoles(Role.SUPER_ADMIN),categoryController.createCategory)
 
 router.get("/",categoryController.getAllCategories)
 
@@ -14,12 +15,12 @@ router.get("/:id", (req, res, next) => {
 	categoryController.getCategory(req, res).catch(next);
 });
 
-router.put("/:id", verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
+router.put("/:id", verifyToken, verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
 	categoryController.updateCategory(req, res).catch(next);
 });
 
-router.delete("/:id", verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
-	categoryController.deleteCategory(req, res).catch(next);
+router.delete("/:id", verifyToken, verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
+	categoryController.deleteCategory(req, res).then(() => {}).catch(next);
 });
 
 
