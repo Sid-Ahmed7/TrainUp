7689e1efb01e400c8c2dd0b6f648f332b60b940a
Sid-Ahmed7 <sido91080@hotmail.fr>
2025-07-22 04:50:57 +0200

    update index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import authRoutes from './authRoutes';
-import categoryRoutes from './categoryRoutes';
-import targetRoutes from './targetRoutes';
-import typeExerciceRoutes from './typeExerciceRoutes'
-import equipmentRoutes from './equipmentRoutes'
-import typeExerciceEquipmentRoutes from './typeExerciceEquipmentRoutes'
-import challengeRoutes from './challengeRoutes'
+import authRoutes from './auth.routes';
+import categoryRoutes from './category.routes';
+import targetRoutes from './target.routes';
+import typeExerciceRoutes from './typeExercice.routes'
+import equipmentRoutes from './equipment.routes'
+import typeExerciceEquipmentRoutes from './typeExerciceEquipment.routes'
+import challengeRoutes from './challenge.routes'
 export {
   authRoutes,
   categoryRoutes,
