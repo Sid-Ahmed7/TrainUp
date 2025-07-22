a2cd5fc8c22767f79533ad8ece467bb274d03628
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-22 21:02:42 +0200

    again

--- src/routes/typeExercice.routes.ts
+++ src/routes/typeExercice.routes.ts
@@ -1,25 +1,38 @@
-import express from "express"
+import express from "express";
 import * as typeExerciceController from "../controllers/typeExercice.controller";
 import { verifyRoles } from "../middlewares/verifyRoles";
 import { verifyToken } from "../middlewares/auth";
 
 import { Role } from "../enums/Role";
 
-const router = express.Router()
+const router = express.Router();
 
-
-
-router.post("/nouveau",verifyToken,verifyRoles(Role.SUPER_ADMIN), typeExerciceController.createTypeExercice)
-router.get("/",typeExerciceController.getAllTypeExercices)
+router.post(
+  "/new",
+  verifyToken,
+  verifyRoles(Role.SUPER_ADMIN),
+  typeExerciceController.createTypeExercice
+);
+router.get("/", typeExerciceController.getAllTypeExercices);
 router.get("/:id", (req, res, next) => {
-    typeExerciceController.getTypeExercice(req, res).catch(next);
+  typeExerciceController.getTypeExercice(req, res).catch(next);
 });
-router.put("/:id",verifyToken, verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
+router.put(
+  "/:id",
+  verifyToken,
+  verifyRoles(Role.SUPER_ADMIN),
+  (req, res, next) => {
     typeExerciceController.updateTypeExercice(req, res).catch(next);
-});
+  }
+);
 
-router.delete("/:id",verifyToken, verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
+router.delete(
+  "/:id",
+  verifyToken,
+  verifyRoles(Role.SUPER_ADMIN),
+  (req, res, next) => {
     typeExerciceController.deleteTypeExercice(req, res).catch(next);
-});
+  }
+);
 
 export default router;
--- src/routes/typeExerciceEquipment.routes.ts
+++ src/routes/typeExerciceEquipment.routes.ts
@@ -1,24 +1,35 @@
-import express from "express"
+import express from "express";
 import * as typeExerciceEquipmentController from "../controllers/typeExerciceEquipment.controller";
 import { verifyRoles } from "../middlewares/verifyRoles";
 import { verifyToken } from "../middlewares/auth";
 
 import { Role } from "../enums/Role";
 
-const router = express.Router()
+const router = express.Router();
 
+router.post(
+  "/new",
+  verifyToken,
+  verifyRoles(Role.SUPER_ADMIN),
+  typeExerciceEquipmentController.createTypeExerciceEquipment
+);
+router.get("/", typeExerciceEquipmentController.getAllTypeExerciceEquipment);
 
-
-router.post("/nouveau",verifyToken,verifyRoles(Role.SUPER_ADMIN), typeExerciceEquipmentController.createTypeExerciceEquipment)
-router.get("/",typeExerciceEquipmentController.getAllTypeExerciceEquipment)
-
-
-
-router.delete("/equipment/:id",verifyToken, verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
+router.delete(
+  "/equipment/:id",
+  verifyToken,
+  verifyRoles(Role.SUPER_ADMIN),
+  (req, res, next) => {
     typeExerciceEquipmentController.deleteByEquipmentId(req, res).catch(next);
-});
-router.delete("/exrcice/:id",verifyToken, verifyRoles(Role.SUPER_ADMIN), (req, res, next) => {
+  }
+);
+router.delete(
+  "/exercice/:id",
+  verifyToken,
+  verifyRoles(Role.SUPER_ADMIN),
+  (req, res, next) => {
     typeExerciceEquipmentController.deleteByExerciceId(req, res).catch(next);
-});
+  }
+);
 
 export default router;
