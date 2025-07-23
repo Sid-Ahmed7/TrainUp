134a4de36034772b2b0cbe7dcb8b7388b4f17ee5
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-23 10:27:12 +0200

    change targetService

--- src/controllers/target.controller.ts
+++ src/controllers/target.controller.ts
@@ -1,62 +1,56 @@
 import { plainToClass } from "class-transformer";
 import { Request, Response } from "express";
-import * as TargetService from "../services/target.service";
+import { TargetService } from "../services/target.service";
 import { CreateTargetDTO } from "../DTO/Target/createTarget.dto";
 import { UpdateTargetDTO } from "../DTO/Target/updateTarget.dto";
 
-
 export class TargetController {
+  static async createTarget(req: Request, res: Response) {
+    const dto = plainToClass(CreateTargetDTO, req.body);
 
-    static async createTarget(req: Request, res: Response) {
-        const dto = plainToClass(CreateTargetDTO, req.body);
+    try {
+      const target = await TargetService.createTarget(dto);
+      res.status(201).json(target);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
 
-        try {
-            const target = await TargetService.createTarget(dto);
-            res.status(201).json(target);
+  static async getAllTargets(req: Request, res: Response) {
+    const target = await TargetService.findAllTarget();
+    res.status(200).json(target);
+  }
 
-        } catch(error: any) {
-            res.status(500).json({message: error.message})
-        }
+  static async getTarget(req: Request, res: Response) {
+    const target = await TargetService.findTargetById(Number(req.params.id));
+
+    if (!target) {
+      return res.status(404).json({ message: "Aucune audience trouvé" });
     }
 
-    static async getAllTargets(req: Request, res: Response) {
-        const target = await TargetService.findAllTarget();
-        res.status(200).json(target);
+    res.status(200).json(target);
+  }
+
+  static async updateTarget(req: Request, res: Response) {
+    const dto = plainToClass(UpdateTargetDTO, req.body);
+    const id = parseInt(req.params.id as string);
+    try {
+      const updatedTarget = await TargetService.updateTarget(id, dto);
+      if (!updatedTarget) {
+        return res.status(404).json({ message: "Aucune audience trouvé" });
+      }
+      return res.status(200).json(updatedTarget);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
     }
+  }
 
-    static async getTarget(req: Request, res: Response){
-        const target = await TargetService.findTargetById(Number(req.params.id))
-        
-        if(!target) {
-            return res.status(404).json({ message: "Aucune audience trouvé" })
-        }
-        
-        res.status(200).json(target);
-    }
-
-
-    static async updateTarget(req: Request, res: Response){
-        const dto = plainToClass(UpdateTargetDTO, req.body);
-        const id = parseInt(req.params.id as string)
-        try {
-            const updatedTarget = await TargetService.updateTarget(id, dto);
-            if (!updatedTarget) {
-                return res.status(404).json({ message: "Aucune audience trouvé"})
-            
-            }
-            return res.status(200).json(updatedTarget);
-        } catch(error: any) {
-            res.status(500).json({message: error.message})
-        }
-    }
-
-    static async deleteTarget(req: Request, res: Response){
+  static async deleteTarget(req: Request, res: Response) {
     const id = parseInt(req.params.id as string);
     const deletedTarget = await TargetService.deleteTarget(id);
-    if (!deletedTarget){
-        return res.status(404).json({ message: "Aucune audience trouvé" });
+    if (!deletedTarget) {
+      return res.status(404).json({ message: "Aucune audience trouvé" });
     }
     res.status(204).send();
-    };
+  }
 }
-
--- src/services/target.service.ts
+++ src/services/target.service.ts
@@ -1,44 +1,33 @@
-import AppDataSource  from "../config/db";
+import AppDataSource from "../config/db";
 import { Target } from "../entities/Target";
 
 const targetRepository = AppDataSource.getRepository(Target);
 
 export class TargetService {
+  static createTarget = async (target: Partial<Target>) => {
+    return targetRepository.save(targetRepository.create(target));
+  };
 
-
-
-
-
-
-    
-}
-export const createTarget = async (target: Partial<Target>) => {
-
-    return targetRepository.save(targetRepository.create(target))
-}
-
-export const updateTarget = async (targetId: number, target: Partial<Target>) => {
-
-    const updatedTarget = await targetRepository.findOneBy({id: targetId })
-    if(!updatedTarget) {
-        return 
+  static updateTarget = async (targetId: number, target: Partial<Target>) => {
+    const updatedTarget = await targetRepository.findOneBy({ id: targetId });
+    if (!updatedTarget) {
+      return;
     }
-    const res =await targetRepository.update(targetId,target)
+    const res = await targetRepository.update(targetId, target);
 
-          return res;
-;
-}   
+    return res;
+  };
 
-export const findAllTarget = () => {
-    return targetRepository.find()
-}
+  static findAllTarget = () => {
+    return targetRepository.find();
+  };
 
-export const findTargetById = (targetId: number) => {
-    return targetRepository.findOneBy({ id: targetId })
-}
+  static findTargetById = (targetId: number) => {
+    return targetRepository.findOneBy({ id: targetId });
+  };
 
-export const deleteTarget = async (targetId: number) => {
+  static deleteTarget = async (targetId: number) => {
     const res = await targetRepository.delete(targetId);
     return res.affected !== 0;
-}   
-
+  };
+}
