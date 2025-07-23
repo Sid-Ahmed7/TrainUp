14071cab5b27518739b635bcca8a43409599ffae
LuzBoger <arthurbrouard2003@gmail.com>
2025-07-23 22:20:33 +0200

    fix: modification equipment response

--- src/services/equipment.service.ts
+++ src/services/equipment.service.ts
@@ -1,32 +1,38 @@
-import AppDataSource  from "../config/db";
+import AppDataSource from "../config/db";
 import { Equipment } from "../entities/Equipment";
 
 const equipmentRepository = AppDataSource.getRepository(Equipment);
 
-
 export const createEquipment = async (equipment: Partial<Equipment>) => {
-    return equipmentRepository.save(equipmentRepository.create(equipment));
-}
+  return equipmentRepository.save(equipmentRepository.create(equipment));
+};
 
-export const updateEquipment = async (equipmentId: number, equipment: Partial<Equipment>) => {
-    const updatedEquipment = await equipmentRepository.findOneBy({id: equipmentId});
-    if(!updatedEquipment) {
-        return null;
-    }
-
-    const res = await equipmentRepository.update(equipmentId, equipment);
-    return res;
-}
-
+export const updateEquipment = async (
+  equipmentId: number,
+  dto: Partial<Equipment>
+) => {
+  const equipmentToUpdate = await equipmentRepository.findOneBy({
+    id: equipmentId,
+  });
+  if (!equipmentToUpdate) {
+    return null;
+  }
+  // Mettre à jour uniquement les champs fournis
+  if (dto.name !== undefined) equipmentToUpdate.name = dto.name;
+  if (dto.description !== undefined)
+    equipmentToUpdate.description = dto.description;
+  await equipmentRepository.save(equipmentToUpdate);
+  return equipmentToUpdate;
+};
 export const findAllEquipments = () => {
-    return equipmentRepository.find();
-}
+  return equipmentRepository.find();
+};
 
-export const findEquipmentById = (equipmentId :number) => {
-    return equipmentRepository.findOneBy({id: equipmentId})
-}
+export const findEquipmentById = (equipmentId: number) => {
+  return equipmentRepository.findOneBy({ id: equipmentId });
+};
 
 export const deleteEquipment = async (equipmentId: number) => {
-    const res = await equipmentRepository.delete(equipmentId);
-    return res.affected !== 0;
-}
+  const res = await equipmentRepository.delete(equipmentId);
+  return res.affected !== 0;
+};
